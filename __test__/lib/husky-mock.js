20d038180cca5210ee0495cccf5ce91ab3f6039d
Tom <tnorth93@gmail.com>
2018-09-26 16:39:27 -0700

    finished get and post routes

---
+++ __test__/lib/husky-mock.js
@@ -0,0 +1,17 @@
+'use strict':
+
+const faker = require('faker');
+const Husky = require('../../mmodel/husky');
+
+const huskyMock = module.exports = {};
+
+huskyMock.pCreateHuskyMock = () => {
+  return new Husky({
+    name: faker.lorem.words(1),
+    description: faker.lorem.words(12),
+  }).save();
+};
+
+huskyMock.pCleanHuskyMocks = () => {
+  return Husky.remove({});
+};
