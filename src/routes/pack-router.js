a5410a366495cd18651f28bed1574a57281ab998
Tom <tnorth93@gmail.com>
2018-10-02 14:21:34 -0700

    created pack mock for testing

--- src/routes/pack-router.js
+++ src/routes/pack-router.js
@@ -8,6 +8,9 @@ const logger = require('../lib/logger');
 const jsonParser = bodyParser.json();
 const router = module.exports = new express.Router();
 
+// ================================================================================
+// POST Create a Pack
+// ================================================================================
 router.post('/api/packs', jsonParser, (request, response, next) => {
   return new Pack(request.body).save()
     .then((savedPack) => {
@@ -17,6 +20,9 @@ router.post('/api/packs', jsonParser, (request, response, next) => {
     .catch(next);
 });
 
+// ================================================================================
+// GET View a Pack
+// ================================================================================
 router.get('/api/packs/:id', (request, response, next) => {
   return Pack.findById(request.params.id)
     .then((pack) => {
@@ -28,4 +34,40 @@ router.get('/api/packs/:id', (request, response, next) => {
       return next(new HttpError(404, 'pack not found'));
     })
     .catch(next);
+});
+
+// ================================================================================
+// DELETE Delete a Pack
+// ================================================================================
+router.delete('/api/packs/:id', (request, response, next) => {
+  return Pack.findByIdAndRemove(request.params.id)
+    .then((pack) => {
+    if (pack) {
+      logger.log(logger.INFO, 'Pack deleted');
+      return response.json(204, pack);
+    }
+    logger.log(logger.INFO, 'Responding with 404 code');
+    return next(new HttpError(404, 'pack not found'));
+  })
+  .catch(next);
+});
+
+// ================================================================================
+// PUT Update a Pack
+// ================================================================================
+router.put('api/packs/:id', (request, response, next) => {
+  const packUpdateOptions = {
+    runValidators: true,
+    new: true,
+  };
+  return Pack.findByIdAndUpdate(request.params.id, request.body, updateOptions)
+    .then((updatedPack) => {
+      if (updatedPack) {
+        logger.log(logger.INFO, 'Responding with 200');
+        return response.json(updatedPack);
+      }
+      logger.log(logger.INFO, 'Responding with 404, Pack not found');
+      return next(new HttpError(404, 'not found'));
+    })
+    .catch(error => next(error));
 });
