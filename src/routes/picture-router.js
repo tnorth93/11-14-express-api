230151f8aab17ab6ab4c2afafc36bfb1ed16ea06
Tom <tnorth93@gmail.com>
2018-10-07 22:23:55 -0700

    mroe tests and get route

--- src/routes/picture-router.js
+++ src/routes/picture-router.js
@@ -6,16 +6,37 @@ const HttpError = require('http-errors');
 const Picture = require('../model/picture');
 const bearerAuthMiddleWare = require('../lib/bearer-auth-middleware');
 
-const jsonParser = bodyParser.json;
+const jsonParser = bodyParser.json();
 const router = module.exports = new express.Router();
 
-router.post('/api/picture', bearerAuthMiddleWare, jsonParser, (request, response, next) => {
+// ===============================================================================================
+// POST
+// ===============================================================================================
+router.post('/api/picture', bearerAuthMiddleWare, (request, response, next) => {
   if (!request.account) {
     return next(new HttpError(400, 'bad request'));
   }
   return new Picture({
     ...request.body,
+    account: request.account._id,
   }).save()
     .then(picture => response.json(picture))
+    .catch(next);
+});
+
+// ===============================================================================================
+// GET
+// ===============================================================================================
+router.get('/api/picture/:id', bearerAuthMiddleWare, jsonParser, (request, response, next) => {
+  if (!request.account) {
+    return next(new HttpError(400, 'bad request'));
+  }
+  return Picture.findById(request.params.id)
+    .then((picture) => {
+      if (picture) {
+        return response.json(picture);
+      }
+      return next(new HttpError(404, 'Picture not found'));
+    })
     .catch(next);
 });
